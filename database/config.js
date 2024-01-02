43ec3cd52379acc994eba05c3d2817ef527717b6
Cristian Avila <avila.cristian420@gmail.com>
2024-01-01 21:55:39 -0600

    Configuracion sobre variantes de entorno en el archivo de database

--- database/config.js
+++ database/config.js
@@ -1,24 +1,21 @@
-import mongoose from "mongoose"
-import 'dotenv';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-//URL de nuestra base de datos
-const url = process.env.MONGOD_ATLAS_CNN; 
+// Configurar dotenv para cargar variables de entorno desde .env
+dotenv.config();
 
-export const dbConection = async() => {
+// URL de nuestra base de datos
+const url = process.env.MONGOD_ATLAS_CNN;
 
+export const dbConection = async () => {
     try {
-        
-        await mongoose.connect( url );
+        await mongoose.connect(url);
 
         console.log('Base de datos online');
-
-
     } catch (error) {
-        //para visualizar que error se esta disparando
+        // Para visualizar qué error se está disparando
         console.log(error);
-        //error controlado 
+        // Error controlado 
         throw new Error('Error a la hora de iniciar la base de datos');
     }
-
-
-}
+};
