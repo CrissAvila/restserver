fbd9a24e1aaa55aafc7c79f63ebe740a6ef7fbfa
Cristian Avila <avila.cristian420@gmail.com>
2024-01-17 17:19:04 -0600

    Se integro el inicio de sesion con google, y actualizaciones en DB para autentificar registros

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ import bcryptjs from "bcryptjs";
 import Usuario from "../models/usuario.js";
 
 import { generarJWT } from "../helpers/generar-jwt.js";
+import { googleVerify } from "../helpers/google-verify.js";
 
 export const loginAuth = async( req = request, res = response) => {
 
@@ -48,5 +49,55 @@ export const loginAuth = async( req = request, res = response) => {
             msg: 'Hable con el administrador'
         });
     }   
+
+};
+
+export const googleSingIn = async(req = request, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        
+        const { correo, nombre, img } = await googleVerify( id_token );
+        
+        //referencia verificar si el correo existe en DB
+        let usuario = await Usuario.findOne({ correo }); 
+        
+        if ( !usuario ) {
+            //Tengo que crearlo 
+            const data = {
+                nombre, 
+                correo, 
+                password: '123456',
+                img,
+                rol: "USER_ROLE",
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        //si el usuario en DB estado: false
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        });
+    };
 
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import { check } from "express-validator";
 
 import { validarCampos } from "../middlewares/validar-campos.js";
 
-import { loginAuth } from "../controllers/auth.js";
+import { googleSingIn, loginAuth } from "../controllers/auth.js";
 
 const routerAuth = Router();
 
@@ -12,6 +12,11 @@ routerAuth.post('/login', [
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ], loginAuth );
+
+routerAuth.post('/google', [
+    check('id_token', 'id_token de google es necesario').not().isEmpty(),
+    validarCampos
+], googleSingIn );
 
 export default routerAuth;
 
